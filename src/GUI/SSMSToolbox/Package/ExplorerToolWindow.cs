5df4ab4301cf676839834a4ac0f86f639466cc13
ErikEJ <ejlskov@hotmail.com>
2016-07-05 17:22:42 +0200

    Adding initial working SSMS extension

---
+++ src/GUI/SSMSToolbox/Package/ExplorerToolWindow.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ExplorerToolWindow.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+using SSMSToolbox;
+
+namespace ErikEJ.SqlCeToolbox
+{
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("f37f5141-198e-4181-9582-7b026ee8f915")]
+    public class ExplorerToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExplorerToolWindow"/> class.
+        /// </summary>
+        public ExplorerToolWindow() : base(null)
+        {
+            this.Caption = "ExplorerToolWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ExplorerToolWindowControl();
+        }
+    }
+}
---
+++ src/GUI/SSMSToolbox/Package/SqlCeToolboxPackage.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+// ReSharper disable once CheckNamespace
+namespace ErikEJ.SqlCeToolbox
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "0.1", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(ExplorerToolWindow), Style = VsDockStyle.Tabbed, Orientation = ToolWindowOrientation.Left, Window = "DocumentWell")]
+    [Guid(PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class SqlCeToolboxPackage : Package
+    {
+        /// <summary>
+        /// ExplorerToolWindowPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "9b80f327-181a-496f-93d9-dcf03d56a792";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExplorerToolWindow"/> class.
+        /// </summary>
+        public SqlCeToolboxPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            OtherWindowsCommand.Initialize(this);
+            ViewMenuCommand.Initialize(this);
+            base.Initialize();            
+        }
+        #endregion
+    }
+}
