116bc005a6f1dab0263dadf98a6a740da259f21f
Erik <Erik@DESKTOP-F14GQSD>
2016-10-29 15:29:48 +0200

    Initial add of new Vsix project with vsix2 manifest

--- src/GUI/SqlCe35Toolbox/Properties/AssemblyInfo.cs
+++ src/GUI/SqlCe35Toolbox/Properties/AssemblyInfo.cs
@@ -1,24 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 [assembly: AssemblyTitle("ErikEJ.SqlCeToolbox")]
 [assembly: AssemblyDescription("Visual Studio Add-in")]
 [assembly: AssemblyProduct("ErikEJ.SqlCeToolbox")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
-
 [assembly: ComVisible(false)]
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page, 
-    // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page, 
-    // app, or any theme specific resource dictionaries)
-)]
-
-[assembly: AssemblyVersion("4.5.0.6")]
-[assembly: AssemblyFileVersion("4.5.0.6")]
-[assembly: AssemblyInformationalVersion("4.6-alpha")]
+[assembly: AssemblyVersion("4.5.0.7")]
+[assembly: AssemblyFileVersion("4.5.0.7")]
+[assembly: AssemblyInformationalVersion("4.6-beta")]
 
