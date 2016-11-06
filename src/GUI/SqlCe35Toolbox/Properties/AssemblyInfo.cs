32ca1e6c24730e34e2ed371b2a7e01f073f8da35
erikej <erikej@VS15WIN10>
2016-11-06 15:39:37 +0000

    Update to latest VS 15 RC and RC build tools

--- src/GUI/SqlCe35Toolbox/Properties/AssemblyInfo.cs
+++ src/GUI/SqlCe35Toolbox/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ErikEJ.SqlCeToolbox")]
-[assembly: AssemblyDescription("Visual Studio Add-in")]
+[assembly: AssemblyDescription("Visual Studio / SSMS Add-in")]
 [assembly: AssemblyProduct("ErikEJ.SqlCeToolbox")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.5.0.9")]
-[assembly: AssemblyFileVersion("4.5.0.9")]
+[assembly: AssemblyVersion("4.5.1000.1")]
+[assembly: AssemblyFileVersion("4.5.1000.1")]
 [assembly: AssemblyInformationalVersion("4.6-beta")]
 
