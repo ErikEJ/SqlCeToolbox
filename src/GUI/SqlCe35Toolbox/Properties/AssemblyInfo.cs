e0746db03b646f4ebb826f11f9b6d22ecdf533b3
erikej <erikej@VS15WIN10>
2016-11-11 12:59:00 +0000

    Refresh to lastest VS build and build tools

--- src/GUI/SqlCe35Toolbox/Properties/AssemblyInfo.cs
+++ src/GUI/SqlCe35Toolbox/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ErikEJ.SqlCeToolbox")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.5.1000.1")]
-[assembly: AssemblyFileVersion("4.5.1000.1")]
+[assembly: AssemblyVersion("4.5.1000.2")]
+[assembly: AssemblyFileVersion("4.5.1000.2")]
 [assembly: AssemblyInformationalVersion("4.6-beta")]
 
