9e6420feaf03071ae940b0f21f508c655c76ef81
ErikEJ <ejlskov@hotmail.com>
2017-02-23 07:34:44 +0100

    Fix Table picker bug

--- src/GUI/SqlCe35Toolbox/Commands/DatabaseMenuCommandsHandler.cs
+++ src/GUI/SqlCe35Toolbox/Commands/DatabaseMenuCommandsHandler.cs
@@ -1258,7 +1258,7 @@ namespace ErikEJ.SqlCeToolbox.Commands
 
             try
             {
-                var ptd = new PickTablesDialog {ExcludeTables = false};
+                var ptd = new PickTablesDialog {IncludeTables = true};
                 using (var repository = DataConnectionHelper.CreateRepository(new DatabaseInfo { ConnectionString = databaseInfo.DatabaseInfo.ConnectionString, DatabaseType = databaseInfo.DatabaseInfo.DatabaseType }))
                 {
                     ptd.Tables = repository.GetAllTableNamesForExclusion();
