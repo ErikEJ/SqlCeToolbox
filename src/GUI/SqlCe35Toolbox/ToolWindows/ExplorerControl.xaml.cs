02d55d170416d0e5ea141acb0f7bb2f48c1c8eb4
ErikEJ <ejlskov@hotmail.com>
2017-04-16 18:19:09 +0200

    Remove some TODOs and improve SSMS version detection

--- src/GUI/SqlCe35Toolbox/ToolWindows/ExplorerControl.xaml.cs
+++ src/GUI/SqlCe35Toolbox/ToolWindows/ExplorerControl.xaml.cs
@@ -162,7 +162,7 @@ namespace ErikEJ.SqlCeToolbox.ToolWindows
                 {
                     Telemetry.Initialize(dte,
                         Assembly.GetExecutingAssembly().GetName().Version.ToString(),
-                        SqlCeToolboxPackage.TelemetryVersion.ToString(),
+                        package.TelemetryVersion.ToString(),
                         "d4881a82-2247-42c9-9272-f7bc8aa29315");
                 }
 #if SSMS
