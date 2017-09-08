523415e3fc6226fcad591d8f64c735182871c116
ErikEJ <ejlskov@hotmail.com>
2017-09-08 18:21:32 +0200

    Adding test project for DGML parser (and more?)

---
+++ src/GUI/UnitTests/DgmlTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class DgmlTest
+    {
+
+        [Test]
+        public void ParseDebugViewSample1()
+        {
+            var sample = File.ReadAllText("Vax.txt");
+        }
+    }
+}
