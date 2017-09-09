d13e2a5eae7da3607b7b4e44c93e501437a70234
ErikEJ <ejlskov@hotmail.com>
2017-09-09 21:48:46 +0200

    WIP - DGML creation now works for Nodes (Navigation links TODO)

---
+++ src/GUI/ReverseEngineer20/ModelAnalyzer/DebugViewParserResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ReverseEngineer20.ModelAnalyzer
+{
+    public class DebugViewParserResult
+    {
+        public DebugViewParserResult()
+        {
+            Nodes = new List<string>();
+            Links = new List<string>();
+        }
+
+        public List<string> Nodes { get; set; }
+
+        public List<string> Links { get; set; }
+    }
+}
