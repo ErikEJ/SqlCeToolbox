aba7eced86e7b790d027e67ca37c4c86700830f6
ErikEJ <ejlskov@hotmail.com>
2017-11-27 14:26:36 +0100

    EFPT: Typo

--- src/GUI/EFCorePowerTools/Helpers/AssemblyBindingRedirectHelper.cs
+++ src/GUI/EFCorePowerTools/Helpers/AssemblyBindingRedirectHelper.cs
@@ -44,7 +44,7 @@ namespace ErikEJ.SqlCeToolbox.Helpers
                 new BindingRedirect
                 {
                     ShortName = "System.Diagnostics.DiagnosticSource",
-                    RedirectToVersion = "4.0.2.0",
+                    RedirectToVersion = "4.0.2.1",
                     PublicKeyToken = "cc7b13ffcd2ddd51"
                 }
             };
