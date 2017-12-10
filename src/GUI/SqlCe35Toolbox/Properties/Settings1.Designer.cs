88e7848b4e483006ed989c732131f5f5521d5710
Sveinn Erlendsson <svakinn@hotmail.com>
2017-12-10 04:14:56 +0000

    Change the manifest name since the project does not compile with spaces and backslash in the Product Name

--- src/GUI/SqlCe35Toolbox/Properties/Settings1.Designer.cs
+++ src/GUI/SqlCe35Toolbox/Properties/Settings1.Designer.cs
@@ -12,7 +12,7 @@ namespace ErikEJ.SqlCeToolbox {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -296,6 +296,18 @@ namespace ErikEJ.SqlCeToolbox {
             }
             set {
                 this["GetObjectExplorerDatabases"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public string ValidateConnectionsOnStart {
+            get {
+                return ((string)(this["ValidateConnectionsOnStart"]));
+            }
+            set {
+                this["ValidateConnectionsOnStart"] = value;
             }
         }
     }
