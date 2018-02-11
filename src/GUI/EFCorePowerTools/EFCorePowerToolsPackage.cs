22db633d8c12e2691d57cc047663cfb50dae83d8
ErikEJ <ejlskov@hotmail.com>
2018-02-11 13:19:34 +0100

    Update to system.data.sqlite 1.0.107 Clean up some projct files and content Require VS 15.3 in EF Core Power Tools

--- src/GUI/EFCorePowerTools/EFCorePowerToolsPackage.cs
+++ src/GUI/EFCorePowerTools/EFCorePowerToolsPackage.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading;
-using System.Threading.Tasks;
 
 using EFCorePowerTools.Extensions;
 using EFCorePowerTools.Handlers;
