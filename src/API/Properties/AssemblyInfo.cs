e39351389a92a88e4a249821f27831c59b15f144
ErikEJ <ejlskov@hotmail.com>
2018-09-24 18:13:19 +0200

    Update to SQLite ADO.NET provider .109
    fixes #641, #734

--- src/API/Properties/AssemblyInfo.cs
+++ src/API/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 
 [assembly:ComVisible(false)]
 [assembly: AssemblyVersion("3.5.2.15")]
-[assembly: AssemblyFileVersion("3.5.2.77")]
-[assembly: AssemblyInformationalVersion("3.5.2.77")]
+[assembly: AssemblyFileVersion("3.5.2.78")]
+[assembly: AssemblyInformationalVersion("3.5.2.78")]
