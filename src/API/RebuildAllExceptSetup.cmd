a3dcd07e5793b62e651ac8b5c4f748819c2ae61d
Erik Ejlskov Jensen <eej@delegate.dk>
2022-12-29 13:38:45 +0100

    Update to latest scripting lib, with a fix for #823

--- src/API/RebuildAllExceptSetup.cmd
+++ src/API/RebuildAllExceptSetup.cmd
@@ -1,6 +1,6 @@
 set ORIGINAL_DIR=%CD% 
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\vsdevcmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\vsdevcmd.bat"
 
 chdir /d %ORIGINAL_DIR% 
 
@@ -20,13 +20,13 @@ cd lib
 cd ..
 pause
 
-msbuild ExportSQLCE31.sln /t:Rebuild /p:Configuration=Release
-copy bin\Release\*.dll ..\..\..\
-copy bin\x86\Release\*.dll ..\..\..\
-cd lib
-.\ilmerge /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:..\..\..\..\ExportSqlCe31.exe ..\bin\x86\release\exportsqlce31.exe QuickGraph.dll QuickGraph.Data.dll QuickGraph.GraphViz.dll
-cd ..
-pause
+rem msbuild ExportSQLCE31.sln /t:Rebuild /p:Configuration=Release
+rem copy bin\Release\*.dll ..\..\..\
+rem copy bin\x86\Release\*.dll ..\..\..\
+rem cd lib
+rem .\ilmerge /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:..\..\..\..\ExportSqlCe31.exe ..\bin\x86\release\exportsqlce31.exe QuickGraph.dll QuickGraph.Data.dll rem QuickGraph.GraphViz.dll
+rem cd ..
+rem pause
 
 msbuild ExportSQLCE40.sln /t:Rebuild /p:Configuration=Release
 copy bin\Release\*.dll ..\..\..\
