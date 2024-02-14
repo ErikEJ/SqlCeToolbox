14b777c7bec63d773cb82562c301599a3536258e
Erik Ejlskov Jensen <eej@delegate.dk>
2024-02-14 14:42:12 +0100

    Update GUI with latest scripting lib

--- src/API/Properties/AssemblyInfo.cs
+++ src/API/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 
 [assembly:ComVisible(false)]
 [assembly: AssemblyVersion("3.5.2.15")]
-[assembly: AssemblyFileVersion("3.5.2.95")]
-[assembly: AssemblyInformationalVersion("3.5.2.95")]
+[assembly: AssemblyFileVersion("3.5.2.96")]
+[assembly: AssemblyInformationalVersion("3.5.2.96")]
