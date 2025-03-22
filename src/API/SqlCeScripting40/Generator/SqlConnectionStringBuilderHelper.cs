fc82393852b0be5a6dea8270394c633dfeac8c27
Erik Ejlskov Jensen <eej@delegate.dk>
2025-03-22 10:03:29 +0100

    Support all MDS keyword replacements

--- src/API/SqlCeScripting40/Generator/SqlConnectionStringBuilderHelper.cs
+++ src/API/SqlCeScripting40/Generator/SqlConnectionStringBuilderHelper.cs
@@ -12,7 +12,13 @@ namespace ErikEJ.SqlCeScripting.Generator
 
         private string ReplaceMdsKeywords(string connectionString)
         {
+            connectionString = connectionString.Replace("Application Intent=", "ApplicationIntent=");
+            connectionString = connectionString.Replace("Connect Retry Count=", "ConnectRetryCount=");
+            connectionString = connectionString.Replace("Connect Retry Interval=", "ConnectRetryInterval=");
+            connectionString = connectionString.Replace("Pool Blocking Period=", "PoolBlockingPeriod=");
             connectionString = connectionString.Replace("Multiple Active Result Sets=", "MultipleActiveResultSets=");
+            connectionString = connectionString.Replace("Multi Subnet Failover=", "MultiSubnetFailover=");
+            connectionString = connectionString.Replace("Transparent Network IP Resolution=", "TransparentNetworkIPResolution=");
             connectionString = connectionString.Replace("Trust Server Certificate=", "TrustServerCertificate=");
             return connectionString;
         }
